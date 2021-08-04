c1a89db55439128bcb4853c707db99a1ace60eb0
Mykyta Ivanchenko <mivansevdon@gmail.com>
2021-08-04 23:52:12 +0300

    Some refactoring

--- routes/api.js
+++ routes/api.js
@@ -11,36 +11,45 @@
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
+    .get((req, res) => {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      return res.json([{}]);
     })
     
-    .post(function (req, res){
-      let title = req.body.title;
+    .post((req, res) => {
+      const {title} = req.body;
       //response will contain new book object including atleast _id and title
+      return res.json({_id: '', title: ''});
     })
     
-    .delete(function(req, res){
+    .delete((req, res) => {
       //if successful response will be 'complete delete successful'
+      return res.send('complete delete successful');
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
+    .get((req, res) => {
+      const bookid = req.params.id;
+
+      return res.json({})
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
     
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
+    .post((req, res) => {
+      const bookid = req.params.id;
+      const {comment} = req.body;
+
       //json res format same as .get
+      return res.json({});
     })
     
-    .delete(function(req, res){
+    .delete((req, res) => {
       let bookid = req.params.id;
+
+      return res.send('delete successful');
       //if successful response will be 'delete successful'
     });
   
